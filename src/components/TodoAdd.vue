6c5919e5b51a39582fff49324f2a582df3de9fd4
Oscar Salas <oscarw8813@gmail.com>
2024-07-31 16:08:33 -0500

    Vue 3 To Do List

---
+++ src/components/TodoAdd.vue
@@ -0,0 +1,61 @@
+<script setup>
+import { ref } from "vue";
+import { useStore } from "vuex";
+import VueFeather from "vue-feather";
+
+defineProps({
+    list: Number,
+});
+const showFormAdd = ref(false);
+const title = ref("");
+const store = useStore();
+
+function toggleFormAdd() {
+    showFormAdd.value = !showFormAdd.value;
+}
+function addItem(list) {
+    const payload = {
+        title: title.value,
+        list: list,
+    };
+    store.commit("ADD_ITEM", payload);
+    title.value = "";
+    toggleFormAdd();
+}
+</script>
+
+<template>
+    <button
+        class="bg-gray-100 hover:bg-gray-200 text-gray-600 flex justify-center font-medium rounded-lg px-4 py-2 mt-5 w-full"
+        @click="toggleFormAdd"
+        v-if="!showFormAdd"
+    >
+        <vue-feather type="plus" size="18px" class="self-center mr-2"></vue-feather>
+        Add Item
+    </button>
+    <div class="relative mt-5" v-if="showFormAdd">
+        <input
+            type="text"
+            placeholder="Item title"
+            class="border border-gray-400 focus:border-blue-500 outline-none rounded-lg px-3 py-2 w-full mr-2"
+            autofocus
+            v-model="title"
+            @keyup.enter="addItem(list)"
+        />
+        <div class="flex justify-end mt-2">
+            <button
+                class="bg-gray-100 hover:bg-gray-200 text-gray-500 font-semibold rounded-lg px-4 py-2 mr-2"
+                @click="toggleFormAdd"
+            >
+                Cancel
+            </button>
+            <button
+                class="bg-blue-700 hover:bg-blue-800 disabled:bg-blue-500 disabled:cursor-not-allowed text-white font-semibold rounded-lg px-4 py-2"
+                @click="addItem(list)"
+                :disabled="!title"
+            >
+                Add
+            </button>
+        </div>
+    </div>
+</template>
